45a2bcd3b42c39c0e7f47b89449e98fcd95f344f
leoportasio <leoportasio@gmail.com>
2023-07-07 15:48:48 -0300

    1ª parte responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,8 @@ function changeCircleColor(color) {
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+
+function toggleMenu () {
+    let menuToggle = document.querySelector('.toggle');
+    menuToggle.classList.toggle('active');
+}
